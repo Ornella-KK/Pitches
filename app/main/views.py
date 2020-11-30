94f56385f980bed0f9c66b5543fd3581b6134847
Ornella-KK <ornellakankazi@gmail.com>
2020-11-30 13:07:06 +0200

    installed psql

---
+++ app/main/views.py
@@ -0,0 +1,41 @@
+from flask import render_template, request, redirect, url_for, abort
+from flask_login import login_required, current_user
+from . forms import PitchForm, CommentForm, CategoryForm
+from .import main
+from .. import db
+from ..models import User, Pitch, Comments, PitchCategory, Votes
+
+#display categories on the landing page
+@main.route('/')
+def index():
+    """
+    View root page function that returns index page
+    """
+    all_category = PitchCategory.get_categories()
+    all_pitches = Pitch.query.order_by('-id').all()
+    print (all_pitches)
+
+    tittle = 'Home- Welcome'
+    return render_template('index.html',title = title, categories = all_category, all_pitches = all_pitches)
+
+#route for adding new pitch
+@main.route('/category/new-pitch/<int:id>', methods = ['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    """
+    Function to check Pitches form and fetch data from the fields 
+    """
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        content = form.content.data
+        new_pitch = Pitch(content = content,category_id = category.id,user_id = current_user.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id=category.id))
+
+    return render_template('new_pitch.html',pitch_form =form, category= category)
+
+#
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     General configuration parent class
     """
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS= False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     #email config
