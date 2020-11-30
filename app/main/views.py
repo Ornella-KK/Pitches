a0734c56a898149247b66eb8d57d0e46ff2b81e7
Ornella-KK <ornellakankazi@gmail.com>
2020-11-30 13:57:42 +0200

    added functions

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
 from flask import render_template, request, redirect, url_for, abort
 from flask_login import login_required, current_user
-from . forms import PitchForm, CommentForm, CategoryForm
+from . forms import PitchForm, CommentForm, UpdateProfile
 from .import main
 from .. import db
-from ..models import User, Pitch, Comments, PitchCategory, Votes
+from ..models import User, Pitch, Comment, Upvote, Downvote
 
 #display categories on the landing page
 @main.route('/')
@@ -11,31 +11,95 @@ def index():
     """
     View root page function that returns index page
     """
-    all_category = PitchCategory.get_categories()
-    all_pitches = Pitch.query.order_by('-id').all()
-    print (all_pitches)
-
-    tittle = 'Home- Welcome'
-    return render_template('index.html',title = title, categories = all_category, all_pitches = all_pitches)
+    posts = Post.query.all()
+    product = Post.query.filter_by(category='product').all()
+    idea = Post.query.filter_by(category='idea').all()
+    business = Post.query.filter_by(category='Business').all()
+    return render_template('index.html', business=business, product=product, idea=idea, posts=posts)
+#....
+@main.route('/posts')
+@login_required
+def posts():
+    posts = Post.query.all()
+    likes = Upvote.query.all()
+    user = current_user
+    return render_template('pitch_display.html', posts=posts, likes=likes, user=user)
 
 #route for adding new pitch
-@main.route('/category/new-pitch/<int:id>', methods = ['GET', 'POST'])
+@main.route('/new_post', methods=['GET', 'POST'])
 @login_required
-def new_pitch(id):
-    """
-    Function to check Pitches form and fetch data from the fields 
-    """
-    form = PitchForm()
-    category = PitchCategory.query.filter_by(id=id).first()
-    if category is None:
-        abort(404)
-
+def new_post():
+    form = PostForm()
     if form.validate_on_submit():
-        content = form.content.data
-        new_pitch = Pitch(content = content,category_id = category.id,user_id = current_user.id)
-        new_pitch.save_pitch()
-        return redirect(url_for('.category', id=category.id))
+        title = form.title.data
+        post = form.post.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        post_obj = Post(post=post, title=title, category=category, user_id=user_id)
+        post_obj.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html', form=form)
 
-    return render_template('new_pitch.html',pitch_form =form, category= category)
+@main.route('/comment/<int:post_id>', methods=['GET', 'POST'])
+@login_required
+def comment(post_id):
+    form = CommentForm()
+    post = Post.query.get(post_id)
+    user = User.query.all()
+    comments = Comment.query.filter_by(post_id=post_id).all()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        post_id = post_id
+        user_id = current_user._get_current_object().id
+        new_comment = Comment(
+            comment=comment,
+            post_id=post_id,
+            user_id=user_id
+        )
+        new_comment.save()
+        new_comments = [new_comment]
+        print(new_comments)
+        return redirect(url_for('.comment', post_id=post_id))
+    return render_template('comment.html', form=form, post=post, comments=comments, user=user)
 
-#
+
+@main.route('/user')
+@login_required
+def user():
+    username = current_user.username
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        return ('not found')
+    return render_template('profile.html', user=user)
+
+
+@main.route('/user/<name>/update_profile', methods=['POST', 'GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username=name).first()
+    if user is None:
+        error = 'The user does not exist'
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile', name=name))
+    return render_template('profile/update_profile.html', form=form)
+
+
+@main.route('/like/<int:id>', methods=['POST', 'GET'])
+@login_required
+def upvote(id):
+    post = Post.query.get(id)
+    vote_mpya = Upvote(post=post, upvote=1)
+    vote_mpya.save()
+    return redirect(url_for('main.posts'))
+
+
+@main.route('/dislike/<int:id>', methods=['GET', 'POST'])
+@login_required
+def downvote(id):
+    post = Post.query.get(id)
+    vm = Downvote(post=post, downvote=1)
+    vm.save()
+    return redirect(url_for('main.posts'))
