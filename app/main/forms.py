72d98d8ed9f4a3a639abaaa32ca832a0a85f58b1
Ornella-KK <ornellakankazi@gmail.com>
2020-11-30 13:44:48 +0200

    added form for pitches,comment and votes

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,19 +6,22 @@ class PitchForm(FlaskForm):
     """
     Class to create a wtf form for creating a pitch
     """
-    content = TextAreaField('ENTER PITCH')
-    submit = SubmitField('SUBMIT')
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('PRODUCT', 'PRODUCT'), ('IDEA', 'IDEA'), ('Business', 'Business')],
+                           validators=[Required()])
+    submit = SubmitField('Post')
 
 class CommentForm(FlaskForm):
     """
     Class to create a wtf form for creating a pitch
     """
-    opinion = TextAreaField('WRITE YOUR COMMENT')
+    opinion = TextAreaField('WRITE YOUR COMMENT',validators=[Required()])
     submit = SubmitField('SUBMIT')
 
-class CategoryForm(FlaskForm):
-    """
-    Class to create a wtf form for creating a pitch
-    """
-    name = StringField('Category Name', validators = [Required()])
-    submit = SubmitField('Create')
+class Vote(FlaskForm):
+    submit = SelectField('Like')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('bio', validators=[Required()])
+    submit = SubmitField('Post')
