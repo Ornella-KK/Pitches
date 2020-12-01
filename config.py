a4eb62281747fe56672e0eb5c9c64573c0f65163
Ornella-KK <ornellakankazi@gmail.com>
2020-12-01 03:49:13 +0200

    changed mailport

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
 
     #email config
     MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
+    MAIL_PORT = 465
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
